54912062c86cf5a123191f9a941171a05bc1b412
workstation <elcirculoti@gmail.com>
2021-05-26 01:53:12 -0400

    primer commit de la app

---
+++ map.js
@@ -0,0 +1,30 @@
+mapboxgl.accessToken = ${{ secret.MAPA }}
+
+navigator.geolocation.getCurrentPosition(exito, error, { enableHighAccuracy:  true });
+
+function exito(position) {
+	console.log(position);
+	confMap([ position.coords.longitude, position.coords.latitude ]);
+}
+
+function error() {
+	confMap([-79.4512, 43.6568]);
+}
+
+function confMap(center) {
+	const map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: center,
+		zoom: 12
+	});
+
+	const nav = new mapboxgl.NavigationControl();
+	map.addControl(nav);
+
+	const direcciones = new MapboxDirections({
+		accessToken: mapboxgl.accessToken
+	}); 
+
+	map.addControl(direcciones, 'top-left');
+}
